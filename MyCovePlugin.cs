56c143c4751ad02b2aebeb7b54fba2e0d3d2b724
Your Name <50252724+DrMeepso@users.noreply.github.com>
2024-12-01 02:50:01 +1300

    Initial commit

---
+++ MyCovePlugin.cs
@@ -0,0 +1,19 @@
+﻿using Cove.Server.Plugins;
+using Cove.Server;
+
+// Change the namespace and class name!
+namespace MyCovePlugin
+{
+    public class MyCovePlugin : CovePlugin
+    {
+        public MyCovePlugin(CoveServer server) : base(server) { }
+
+        public override void onInit()
+        {
+            base.onInit();
+
+            Log("Hello world!");
+        }
+
+    }
+}
